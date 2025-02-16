26ca2cb4ab7340b0be7d16c922af467965ccc011
Kaue Lima <kauelyma@gmail.com>
2025-02-16 15:45:56 -0300

    Adiciona função para exibi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string mensagemDeBoaVidas = "Boas vindas, Hello, World";
+
+void ExibirMensagemDeBoasVindas()
+{
+    
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+
+");
+    Console.WriteLine(mensagemDeBoaVidas);
+}
+
+void ExibirMenuOpções ()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma bandas");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar as bandas");
+    Console.WriteLine("Digite 4 para fazer as medias das bandas");
+    Console.WriteLine("Digite 5 para sair");
+}
+ExibirMensagemDeBoasVindas();
+ExibirMenuOpções();
+
